206b884ba09ec42fd7e3afdd42379bf6403d8080
Jeff Newcomer <jrnewk@gmail.com>
2018-10-08 02:03:06 -0700

    Sort events by date and remove events in the past.

--- front-page.php
+++ front-page.php
@@ -17,9 +17,21 @@
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
       <?php
+        $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
-          'posts_per_page' => 2,
-          'post_type' => 'event'
+          'posts_per_page' => -1,
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          )
         ));
 
         while($homepageEvents->have_posts()) {
