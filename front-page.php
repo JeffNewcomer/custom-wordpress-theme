b80c94266e30a7f30a33eb671d57c1b284fb23db
Jeff Newcomer <jrnewk@gmail.com>
2018-10-08 01:43:00 -0700

    Add custom field for entering event dates.

--- front-page.php
+++ front-page.php
@@ -26,8 +26,11 @@
           $homepageEvents->the_post(); ?>
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
+              <span class="event-summary__month"><?php
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate->format('M')
+              ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
