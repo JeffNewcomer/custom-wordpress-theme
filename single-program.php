c2991d978525c0144ba23a82ea21ecf1e24a9103
Jeff Newcomer <jrnewk@gmail.com>
2018-10-10 22:24:41 -0700

    Add professors and create relationship to their related programs (subjects taught).

--- single-program.php
+++ single-program.php
@@ -22,6 +22,34 @@
       <div class="generic-content"><?php the_content(); ?></div>
 
       <?php
+
+      $relatedProfessors = new WP_Query(array(
+        'posts_per_page' => -1,
+        'post_type' => 'professor',
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"'
+          )
+        )
+      ));
+
+      if ($relatedProfessors->have_posts()) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+        while($relatedProfessors->have_posts()) {
+          $relatedProfessors->the_post(); ?>
+          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        }
+
+        // this code below needs to be added between the two queries when you run multiple custom queries on a single page
+        wp_reset_postdata();
+
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
           'posts_per_page' => 2,
