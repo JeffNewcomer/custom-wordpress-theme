cc24a624e88eb4e72cde9f0405f1b9bc1a2eee54
Jeff Newcomer <jrnewk@gmail.com>
2018-10-11 00:26:16 -0700

    Create pageBanner function to reduce duplicate code.

--- functions.php
+++ functions.php
@@ -1,5 +1,37 @@
 <?php
 
+function pageBanner($args = NULL) {
+
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image')) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else{
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+
+  }
+
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php
+}
+
 function university_files() {
   // changing the version from 1.0 to microtime() on the js and css links forces chrome to look at the file every time instead of using a cached version. Note: when you make the site live you'll want to change this back
   wp_enqueue_script('main-university-javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
--- page.php
+++ page.php
@@ -3,17 +3,11 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
+    the_post();
+    pageBanner();
+      ?>
 
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>DON'T FORGET TO REPLACE ME LATER</p>
-        </div>
-      </div>
-    </div>
+
 
     <div class="container container--narrow page-section">
 
