bda59c47d79295f5cae04774aeb7eabbfac2995b
Jeff Newcomer <jrnewk@gmail.com>
2018-10-16 02:30:13 -0700

    Add search overlay for search results.

--- footer.php
+++ footer.php
@@ -68,7 +68,15 @@
 
     </div>
   </footer>
-
+  <div class="search-overlay">
+    <div class="search-ovarlay__top">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+  </div>
 <?php wp_footer(); ?>
 </body>
 </html>
