5d72e7b3a5f11e9223274a0035fe0cdd39ab64da
Jeff Newcomer <jrnewk@gmail.com>
2018-10-16 00:36:11 -0700

    Add maps to individual campus pages.

--- archive-campus.php
+++ archive-campus.php
@@ -17,10 +17,11 @@ pageBanner(array(
     $mapLocation = get_field('map_location');
     ?>
     <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php
-      echo $mapLocation['lng']; ?>"></div>
-  <?php }
-  echo paginate_links();
-?>
+      echo $mapLocation['lng']; ?>">
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <?php echo $mapLocation['address']; ?>
+    </div>
+  <?php } ?>
 </div>
 
 </div>
