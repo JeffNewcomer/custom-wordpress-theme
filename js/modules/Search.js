c0b425a1619bdebabfc194ee39a08151b82d1d06
Jeff Newcomer <jrnewk@gmail.com>
2018-10-16 21:29:30 -0700

    Add ability to open search by typing S and close search by typing ESC.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -7,19 +7,37 @@ class Search{
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
   //2. Events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
   }
 
   // 3. methods
+  keyPressDispatcher(e) {
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    console.log("our open methood just ran");
+    this.isOverlayOpen = true;
   }
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    console.log("our close methood just ran");
+    this.isOverlayOpen = false;
   }
 }
 
