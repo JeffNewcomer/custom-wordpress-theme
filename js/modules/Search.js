fb94136ff8f323b4806358446e9ac8c465c81494
Jeff Newcomer <jrnewk@gmail.com>
2018-10-16 22:03:18 -0700

    Add delay in search field that waits for the user to pause their typing.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,17 +6,26 @@ class Search{
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
+
   //2. Events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
 
   // 3. methods
+  typingLogic() {
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function () {console.log("timeout test");}, 2000);
+  }
+
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay();
