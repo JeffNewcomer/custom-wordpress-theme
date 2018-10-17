2a1d844cf03eb7aea6a79ab159eeafc39848f1f9
Jeff Newcomer <jrnewk@gmail.com>
2018-10-17 01:01:30 -0700

    Add post search feature that returns actual links to the posts found.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -47,8 +47,13 @@ class Search{
   }
 
   getResults() {
-    $.getJSON('http://newk-university.local/wp-json/wp/v2/pages?search', function(posts) {
-      alert(posts[0].title.rendered);
+    $.getJSON('http://newk-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+      this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        <ul class="link-list min-list">
+          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        </ul>
+      `);
     });
   }
 
