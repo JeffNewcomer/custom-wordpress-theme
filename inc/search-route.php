b09f5d9b48edfc66eebe1e6307c767ab1ad652f3
Jeff Newcomer <jrnewk@gmail.com>
2018-10-18 00:15:28 -0700

    Create new custom rest API url.

---
+++ inc/search-route.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch() {
+  register_rest_route('university/v1', 'search', array(
+    'methods' => WP_REST_SERVER::READABLE,
+    'callback' => 'universitySearchResults'
+  ));
+}
+
+function universitySearchResults() {
+  return 'Congrats';
+}
