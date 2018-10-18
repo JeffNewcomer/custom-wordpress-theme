80b34eee6d27804ed5510b81124eeca7abc122e9
Jeff Newcomer <jrnewk@gmail.com>
2018-10-17 21:04:28 -0700

    Add text to let the user know when no search results come back.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -50,9 +50,9 @@ class Search{
     $.getJSON('http://newk-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
       this.resultsDiv.html(`
         <h2 class="search-overlay__section-title">General Information</h2>
-        <ul class="link-list min-list">
+        ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
           ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-        </ul>
+        ${posts.length ? '</ul>' : ''}
       `);
     });
   }
