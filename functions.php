c48770bca0a11ba65cd227cd4c8d6edc032ff128
Jeff Newcomer <jrnewk@gmail.com>
2018-10-17 23:31:28 -0700

    Add author name to all search results that are posts.

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function university_custom_rest() {
+  register_rest_field('post', 'authorName', array(
+    'get_callback' => function() {return get_the_author();}
+  ));
+}
+
+add_action('rest_api_init', 'university_custom_rest');
+
 function pageBanner($args = NULL) {
 
   if (!$args['title']) {
