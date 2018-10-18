2f8d9b477e9d1a6429634cfbbe3dfc8729bd53eb
Jeff Newcomer <jrnewk@gmail.com>
2018-10-17 21:38:59 -0700

    Move html for search overlay from footer.php to search.js.

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class Search{
   // 1. describe and create/initiate our object
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results")
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -80,6 +81,25 @@ class Search{
     console.log("our close methood just ran");
     this.isOverlayOpen = false;
   }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-ovarlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div class="container">
+          <div id="search-overlay__results"</div>
+        </div>
+      </div>
+    `);
+  }
+
 }
 
 export default Search;
