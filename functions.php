918e66cf590264ed527c56c81842cd776ff8951c
Jeff Newcomer <jrnewk@gmail.com>
2018-10-17 21:29:58 -0700

    Made changes to search to allow theme to be used universally as opposd to only locally, and reset spinner for new searches.

--- functions.php
+++ functions.php
@@ -49,7 +49,12 @@ function university_files() {
 
   wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+  //wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_localize_script('main-university-javascript', 'universityData', array(
+    'root_url' => get_site_url()
+  ));
+
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
