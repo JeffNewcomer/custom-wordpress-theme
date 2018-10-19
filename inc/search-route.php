000a55cf725841ce5d38f5a50af58b85a01273dd
Jeff Newcomer <jrnewk@gmail.com>
2018-10-18 19:22:34 -0700

    Add 5 sections to results overlay (General Info, Programs, Campuses, Professors, and Events.

--- inc/search-route.php
+++ inc/search-route.php
@@ -29,35 +29,37 @@ function universitySearchResults($data) {
     if(get_post_type() == 'post' OR get_post_type() == 'page') {
       array_push($results['generalInfo'], array(
         'title' => get_the_title(),
-        'permailink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'postType' => get_post_type(),
+        'authorName' => get_the_author()
       ));
     }
 
     if(get_post_type() == 'professor') {
       array_push($results['professors'], array(
         'title' => get_the_title(),
-        'permailink' => get_the_permalink()
+        'permalink' => get_the_permalink()
       ));
     }
 
     if(get_post_type() == 'program') {
       array_push($results['programs'], array(
         'title' => get_the_title(),
-        'permailink' => get_the_permalink()
+        'permalink' => get_the_permalink()
       ));
     }
 
     if(get_post_type() == 'campus') {
       array_push($results['campuses'], array(
         'title' => get_the_title(),
-        'permailink' => get_the_permalink()
+        'permalink' => get_the_permalink()
       ));
     }
 
     if(get_post_type() == 'event') {
       array_push($results['events'], array(
         'title' => get_the_title(),
-        'permailink' => get_the_permalink()
+        'permalink' => get_the_permalink()
       ));
     }
 
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -48,21 +48,38 @@ class Search{
   }
 
   getResults() {
-    $.when(
-      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
-      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-      ).then((posts, pages) => {
-      var combinedResults = posts[0].concat(pages[0]);
+    $.getJSON(universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val(), (results) => {
       this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">General Information</h2>
-        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
-        ${combinedResults.length ? '</ul>' : ''}
+        <div class="row">
+          <div class="one-third">
+            <h2 class=search-overlay__section-title>General Information</h2>
+            ${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
+              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.postType == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
+            ${results.generalInfo.length ? '</ul>' : ''}
+          </div>
+          <div class="one-third">
+            <h2 class=search-overlay__section-title>Programs</h2>
+            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs match that search. <a href="${universityData.root_url}/programs">View all programs</a></p>`}
+              ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+            ${results.programs.length ? '</ul>' : ''}
+            <h2 class=search-overlay__section-title>Professors</h2>
+
+          </div>
+          <div class="one-third">
+            <h2 class=search-overlay__section-title>Campuses</h2>
+            ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses matches that search. <a href="${universityData.root_url}/campuses">View all campuses</a></p>`}
+              ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+            ${results.campuses.length ? '</ul>' : ''}
+
+
+            <h2 class=search-overlay__section-title>Events</h2>
+
+          </div>
+        </div>
       `);
       this.isSpinnerVisible = false;
-    }, () => {
-      this.resultsDiv.html('<p>Unexpected error; please try again.</p>');
     });
+
   }
 
   keyPressDispatcher(e) {
