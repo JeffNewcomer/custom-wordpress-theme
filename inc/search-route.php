2a34fb87b16723ff411a5c5734c551e03285a1b9
Jeff Newcomer <jrnewk@gmail.com>
2018-10-18 17:32:01 -0700

    Create new custom array for professor results.

--- inc/search-route.php
+++ inc/search-route.php
@@ -10,5 +10,20 @@ function universityRegisterSearch() {
 }
 
 function universitySearchResults() {
-  return 'Congrats';
+  $professors = new WP_Query(array(
+    'post_type' => 'professor'
+  ));
+
+  $professorResults = array();
+
+  while($professors->have_posts()) {
+    $professors->the_post();
+    array_push($professorResults, array(
+      'title' => get_the_title(),
+      'permailink' => get_the_permalink()
+    ));
+  }
+
+  return $professorResults;
+
 }
