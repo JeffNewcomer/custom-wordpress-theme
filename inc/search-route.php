af6fa6621c93fd109b8165c17feeee89e1924882
Jeff Newcomer <jrnewk@gmail.com>
2018-10-19 23:33:31 -0700

    Make searches for any subject also bring in professors of that subject.

--- inc/search-route.php
+++ inc/search-route.php
@@ -78,30 +78,38 @@ function universitySearchResults($data) {
 
   }
 
-  $programRelationshipQuery = new WP_Query(array(
-    'post_type' => 'professor',
-    'meta_query' => array(
-      array(
+  if ($results['programs']) {
+    $programsMetaQuery = array('relation' => 'OR');
+
+    foreach($results['programs'] as $item) {
+      array_push($programsMetaQuery, array(
         'key' => 'related_programs',
         'compare' => 'LIKE',
-        'value' => '"' . $results['programs'][0]['id'] . '"'
-      )
-    )
-  ));
-
-  while($programRelationshipQuery->have_posts()) {
-    $programRelationshipQuery->the_post();
-
-    if(get_post_type() == 'professor') {
-      array_push($results['professors'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        'value' => '"' . $item['id'] . '"'
       ));
     }
+
+    $programRelationshipQuery = new WP_Query(array(
+      'post_type' => 'professor',
+      'meta_query' => $programsMetaQuery
+    ));
+
+    while($programRelationshipQuery->have_posts()) {
+      $programRelationshipQuery->the_post();
+
+      if(get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        ));
+      }
+    }
+
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
   }
 
-  $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
 
   return $results;
 
