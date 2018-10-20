4aa06c2d7f06dec2dbc5f1dcb882ab9ee0444a0d
Jeff Newcomer <jrnewk@gmail.com>
2018-10-19 22:48:45 -0700

    Make searches for biology also bring in professors of biology.

--- inc/search-route.php
+++ inc/search-route.php
@@ -77,6 +77,31 @@ function universitySearchResults($data) {
 
   }
 
+  $programRelationshipQuery = new WP_Query(array(
+    'post_type' => 'professor',
+    'meta_query' => array(
+      array(
+        'key' => 'related_programs',
+        'compare' => 'LIKE',
+        'value' => '"68"'
+      )
+    )
+  ));
+
+  while($programRelationshipQuery->have_posts()) {
+    $programRelationshipQuery->the_post();
+
+    if(get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+      ));
+    }
+  }
+
+  $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
   return $results;
 
 }
