75178c6e5f841c02abc0eec1a0471930fb7fe060
Jeff Newcomer <jrnewk@gmail.com>
2018-10-20 00:11:24 -0700

    Make searches for any subject also bring in campuses that offer that subject.

--- inc/search-route.php
+++ inc/search-route.php
@@ -44,6 +44,19 @@ function universitySearchResults($data) {
     }
 
     if(get_post_type() == 'program') {
+      $relatedCampuses = get_field('related_campus');
+
+      if ($relatedCampuses) {
+        foreach($relatedCampuses as $campus) {
+          array_push($results['campuses'], array(
+            'title' => get_the_title($campus),
+            'permalink' => get_the_permalink($campus)
+          ));
+        }
+      }
+
+
+
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
