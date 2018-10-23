e8542a68d9fb5609a1288394d7a36396d7be2184
Jeff Newcomer <jrnewk@gmail.com>
2018-10-23 00:52:48 -0700

    Add a logout button and hide login when user is already logged in.

--- header.php
+++ header.php
@@ -35,9 +35,18 @@
 
           </nav>
           <div class="site-header__util">
-            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+            <?php if(is_user_logged_in()) { ?>
+              <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small  btn--dark-orange float-left btn--with-photo">
+              <span class="site-header__avatar"><?php echo get_avatar(get_current_user_id(), 60); ?></span>
+              <span class="btn__text">Log Out</span>
+              </a>
 
-            <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+            <?php } else { ?>
+              <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+
+              <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+            <?php } ?>
+
 
             <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
           </div>
