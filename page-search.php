4db7f02c46b31816f6240e5b2caf14032c24cc11
Jeff Newcomer <jrnewk@gmail.com>
2018-10-22 23:01:49 -0700

    Style the search form results for users that have javascript disabled.

--- page-search.php
+++ page-search.php
@@ -51,13 +51,7 @@
     <?php } ?>
 
       <div class="generic-content">
-        <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-          <label class="headline headline--medium" for "s">Perform a New Search:</label>
-          <div class="search-form-row">
-            <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
-            <input class="search-submit" type="submit" value="Search">
-          </div>
-        </form>
+        <?php get_search_form(); ?>
       </div>
 
     </div>
