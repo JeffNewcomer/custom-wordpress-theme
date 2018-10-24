207e30f67a669aa02f5fb1ffc65f85d34d36774e
Jeff Newcomer <jrnewk@gmail.com>
2018-10-24 01:13:23 -0700

    Add edit and delete buttons for each note.

--- page-my-notes.php
+++ page-my-notes.php
@@ -28,6 +28,8 @@
             $userNotes->the_post(); ?>
             <li>
               <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>"/>
+              <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+              <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
               <textarea class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
             </li>
           <?php }
