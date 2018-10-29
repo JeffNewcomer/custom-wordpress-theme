d67845c3a1504efa2d4c011c21baa0774bb6a31c
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 21:24:38 -0700

    Change edit button to a cancel button when user is editing a note. Change note back to read only when user clicks cancel.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -7,14 +7,31 @@ class MyNotes {
 
   events() {
     $(".delete-note").on("click", this.deleteNote);
-    $(".edit-note").on("click", this.editNote);
+    $(".edit-note").on("click", this.editNote.bind(this));
   }
 
   //methods will go here
   editNote(e) {
     var thisNote = $(e.target).parents("li");
+    if (thisNote.data("state") == "editable") {
+      this.makeNoteReadOnly(thisNote);
+    } else {
+      this.makeNoteEditable(thisNote);
+    }
+  }
+
+  makeNoteEditable(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i>Cancel')
     thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
     thisNote.find(".update-note").addClass("update-note--visible");
+    thisNote.data("state", "editable");
+  }
+
+  makeNoteReadOnly(thisNote) {
+    thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i>Edit')
+    thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field");
+    thisNote.find(".update-note").removeClass("update-note--visible");
+    thisNote.data("state", "cancel");
   }
 
   deleteNote(e) {
