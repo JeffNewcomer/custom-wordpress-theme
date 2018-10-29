68d10db189969c38c452eb533a81cf25f8a903e5
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 22:16:00 -0700

    Add newly created notes to the list of current notes below it immediately after the user clicks the create note button.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -100,7 +100,15 @@ class MyNotes {
       data: ourNewPost,
       success: (response) => {
         $(".new-note-title, .new-note-body").val('');
-        $('<li>Imagine real data here</li>').prependTo("#my-notes").hide().slideDown();
+        $(`
+          <li data-id="${response.id}">
+            <input readonly class="note-title-field" value="${response.title.raw}"/>
+            <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+            <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+            <textarea readonly class="note-body-field">${response.content.raw}</textarea>
+            <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
+          </li>
+          `).prependTo("#my-notes").hide().slideDown();
 
         console.log("congrats");
         console.log(response);
