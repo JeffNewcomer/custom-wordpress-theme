1e209fc6d3e078cae21bd9b4fbd616365a9ec770
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 21:36:07 -0700

    Allow user to update a note using the edit and save button.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -8,6 +8,7 @@ class MyNotes {
   events() {
     $(".delete-note").on("click", this.deleteNote);
     $(".edit-note").on("click", this.editNote.bind(this));
+    $(".update-note").on("click", this.updateNote.bind(this));
   }
 
   //methods will go here
@@ -45,6 +46,33 @@ class MyNotes {
       type: 'DELETE',
       success: (response) => {
         thisNote.slideUp();
+        console.log("congrats");
+        console.log(response);
+      },
+      error: (response) => {
+        console.log("sorry");
+        console.log(response);
+      }
+    });
+  }
+
+  updateNote(e) {
+    var thisNote = $(e.target).parents("li");
+
+    var ourUpdatedPost = {
+      'title': thisNote.find(".note-title-field").val(),
+      'content': thisNote.find(".note-body-field").val()
+    }
+
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+      type: 'POST',
+      data: ourUpdatedPost,
+      success: (response) => {
+        this.makeNoteReadOnly(thisNote);
         console.log("congrats");
         console.log(response);
       },
