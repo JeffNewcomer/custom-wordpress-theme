e2a9b8bc40d5a7f5114901e7fc5a1f3f4a8abf44
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 23:13:34 -0700

    Allow subscribers to add notes and change privacy on all notes to be private by default.

--- functions.php
+++ functions.php
@@ -165,3 +165,14 @@ add_filter('login_headertitle', 'ourLoginTitle');
 function ourLoginTitle() {
   return get_bloginfo('name');
 }
+
+// force note posts to be openssl_get_private
+add_filter('wp_insert_post_data', 'makeNotePrivate');
+
+function makeNotePrivate($data) {
+  if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+    $data['post_status'] = "private";
+  }
+
+  return $data;
+}
