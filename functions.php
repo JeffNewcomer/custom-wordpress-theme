a0438d970c7bfa73669272e50388ed2bf45237ea
Jeff Newcomer <jrnewk@gmail.com>
2018-10-29 01:28:25 -0700

    Revert "Add a per user post limit of 10 posts."
    This reverts commit 4648570e1bfae9395ce5c4f1c78fafd7c0d974b9.

--- functions.php
+++ functions.php
@@ -167,14 +167,10 @@ function ourLoginTitle() {
 }
 
 // force note posts to be openssl_get_private
-add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+add_filter('wp_insert_post_data', 'makeNotePrivate');
 
-function makeNotePrivate($data, $postarr) {
+function makeNotePrivate($data) {
   if ($data['post_type'] == 'note') {
-    if(count_user_posts(get_current_user_id(), 'note') >9 AND !$postarr['ID']) {
-      die("You have reached your note limit.");
-    }
-
     $data['post_content'] = sanitize_textarea_field($data['post_content']);
     $data['post_title'] = sanitize_text_field($data['post_title']);
   }
--- page-my-notes.php
+++ page-my-notes.php
@@ -37,7 +37,7 @@
               <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>"/>
               <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
               <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-              <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
+              <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
               <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
             </li>
           <?php }
