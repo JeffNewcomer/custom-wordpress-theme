8a7f01d4f420c79a7deb58ac55afaf714df82ae2
Jeff Newcomer <jrnewk@gmail.com>
2018-10-29 00:53:12 -0700

    Stop Private from automatically being added to the front of note titles. Change code to prevent any html from running in the text area or title area of notes made by subscribers.

--- functions.php
+++ functions.php
@@ -170,6 +170,10 @@ function ourLoginTitle() {
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data) {
+  if ($data['post_type'] == 'note') {
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
   if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
     $data['post_status'] = "private";
   }
--- page-my-notes.php
+++ page-my-notes.php
@@ -34,7 +34,7 @@
           while($userNotes->have_posts()) {
             $userNotes->the_post(); ?>
             <li data-id="<?php the_ID(); ?>">
-              <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>"/>
+              <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>"/>
               <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
               <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
               <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
