4648570e1bfae9395ce5c4f1c78fafd7c0d974b9
Jeff Newcomer <jrnewk@gmail.com>
2018-10-29 01:14:35 -0700

    Add a per user post limit of 10 posts.  subscribers to add notes and change privacy on all notes to be private by default.

--- functions.php
+++ functions.php
@@ -167,10 +167,14 @@ function ourLoginTitle() {
 }
 
 // force note posts to be openssl_get_private
-add_filter('wp_insert_post_data', 'makeNotePrivate');
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
-function makeNotePrivate($data) {
+function makeNotePrivate($data, $postarr) {
   if ($data['post_type'] == 'note') {
+    if(count_user_posts(get_current_user_id(), 'note') >9 AND !$postarr['ID']) {
+      die("You have reached your note limit.");
+    }
+
     $data['post_content'] = sanitize_textarea_field($data['post_content']);
     $data['post_title'] = sanitize_text_field($data['post_title']);
   }
