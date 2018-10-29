ba84dc07889a2f60914acf1c99f0286e3cdfd6c5
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 22:07:24 -0700

    Allow user to create a new note on the notes page and add the note to the list of current notes once page is refreshed.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -9,6 +9,7 @@ class MyNotes {
     $(".delete-note").on("click", this.deleteNote);
     $(".edit-note").on("click", this.editNote.bind(this));
     $(".update-note").on("click", this.updateNote.bind(this));
+    $(".submit-note").on("click", this.createNote.bind(this))
   }
 
   //methods will go here
@@ -73,6 +74,34 @@ class MyNotes {
       data: ourUpdatedPost,
       success: (response) => {
         this.makeNoteReadOnly(thisNote);
+        console.log("congrats");
+        console.log(response);
+      },
+      error: (response) => {
+        console.log("sorry");
+        console.log(response);
+      }
+    });
+  }
+
+  createNote(e) {
+    var ourNewPost = {
+      'title': $(".new-note-title").val(),
+      'content': $(".new-note-body").val(),
+      'status': 'publish'
+        }
+
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/wp/v2/note/',
+      type: 'POST',
+      data: ourNewPost,
+      success: (response) => {
+        $(".new-note-title, .new-note-body").val('');
+        $('<li>Imagine real data here</li>').prependTo("#my-notes").hide().slideDown();
+
         console.log("congrats");
         console.log(response);
       },
