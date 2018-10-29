a393558888cf0013909deef6055d3d00d6bfedd1
Jeff Newcomer <jrnewk@gmail.com>
2018-10-28 20:52:16 -0700

    Make notes read only unless user clicks the edit button. Add a save button for notes that have been edited.

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -7,9 +7,16 @@ class MyNotes {
 
   events() {
     $(".delete-note").on("click", this.deleteNote);
+    $(".edit-note").on("click", this.editNote);
   }
 
   //methods will go here
+  editNote(e) {
+    var thisNote = $(e.target).parents("li");
+    thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+    thisNote.find(".update-note").addClass("update-note--visible");
+  }
+
   deleteNote(e) {
     var thisNote = $(e.target).parents("li");
 
