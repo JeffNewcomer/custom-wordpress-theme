e312c774aeebca5cc0734df85ee9e924cb0c1dac
Jeff Newcomer <jrnewk@gmail.com>
2018-10-29 23:39:19 -0700

    Add heart icon and counter for professors.

--- single-professor.php
+++ single-professor.php
@@ -18,6 +18,11 @@
             <?php the_post_thumbnail('professorPortrait'); ?>
           </div>
           <div class="two-thirds">
+            <span class="like-box">
+              <i class="fa fa-heart-o" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+              <span class="like-count">3</span>
+            </span>
             <?php the_content(); ?>
           </div>
         </div>
