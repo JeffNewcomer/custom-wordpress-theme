dfe283c275b3d0bd01a6072b93c0b21539a4094a
Jeff Newcomer <jrnewk@gmail.com>
2018-10-30 00:10:07 -0700

    Change heart icon to a solid heart if a new like post has been created for that professor.

--- single-professor.php
+++ single-professor.php
@@ -18,10 +18,42 @@
             <?php the_post_thumbnail('professorPortrait'); ?>
           </div>
           <div class="two-thirds">
-            <span class="like-box">
+            <?php
+              $likeCount = new WP_Query(array(
+                'post_type' => 'like',
+                'meta_query'=> array(
+                  array(
+                    'key' => 'liked_professor_id',
+                    'compare' => '=',
+                    'value' => get_the_ID()
+                  )
+                )
+              ));
+
+              $existStatus = 'no';
+
+              $existQuery = new WP_Query(array(
+                'author' => get_current_user_id(),
+                'post_type' => 'like',
+                'meta_query'=> array(
+                  array(
+                    'key' => 'liked_professor_id',
+                    'compare' => '=',
+                    'value' => get_the_ID()
+                  )
+                )
+              ));
+
+              if ($existQuery->found_posts) {
+                $existStatus = 'yes';
+              }
+
+
+            ?>
+            <span class="like-box" data-exists="<?php echo $existStatus; ?>"<>
               <i class="fa fa-heart-o" aria-hidden="true"></i>
               <i class="fa fa-heart" aria-hidden="true"></i>
-              <span class="like-count">3</span>
+              <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
             </span>
             <?php the_content(); ?>
           </div>
