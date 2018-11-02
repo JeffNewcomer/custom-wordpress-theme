57f1d2498b30a283eff815b3f63cfbd5c0a01180
Jeff Newcomer <jrnewk@gmail.com>
2018-11-01 22:57:26 -0700

    Create custom POST and DELETE endpoints.

---
+++ inc/like-route.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action('rest_api_init', 'universityLikeRoutes');
+
+function universityLikeRoutes() {
+  register_rest_route('university/v1', 'manageLike', array(
+    'methods' => 'POST',
+    'callback' => 'createLike'
+  ));
+
+  register_rest_route('university/v1', 'manageLike', array(
+    'methods' => 'DELETE',
+    'callback' => 'deleteLike'
+  ));
+}
+
+function createLike() {
+  return 'thanks for trying to create a like';
+}
+
+function deleteLike() {
+  return 'thanks for trying to delete a like';
+}
--- js/modules/Like.js
+++ js/modules/Like.js
@@ -21,11 +21,29 @@ class Like {
   }
 
   createLike() {
-    alert("create test message");
+    $.ajax({
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'POST',
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    });
   }
 
   deleteLike() {
-    alert("delete test message");
+    $.ajax({
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'DELETE',
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    });
   }
 }
 
